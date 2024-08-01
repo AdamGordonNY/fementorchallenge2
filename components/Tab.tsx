e607c99ea25af2e7977a081e0626f3978ea154ac
adam <adamg42685@gmail.com>
2024-07-31 23:26:53 -0400

    front end mentor challenge 2 init

---
+++ components/Tab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tab = () => {
+  return (
+    <div className="background text-black font-bold w-[80px] h-[40px] inline-flex rounded-lg items-center justify-center py-3">
+      Learning
+    </div>
+  );
+};
+
+export default Tab;
